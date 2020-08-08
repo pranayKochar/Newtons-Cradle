00130824a72be4c44c836a0d0b21e5d34caee994
pranayKochar <64725229+pranayKochar@users.noreply.github.com>
2020-08-08 14:04:10 +0530

    Added Files

--- bob.js
+++ bob.js
@@ -1,7 +1,7 @@
 class Bob{
     constructor(x, y,radius) {
         var options = {
-            'isStatic': false,
+            'isStatic': true,
             'restitution':0.3,
             'friction':0.5,
             'density':1.2,
